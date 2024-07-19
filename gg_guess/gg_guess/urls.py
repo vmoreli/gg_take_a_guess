cec62deee854690bab75551516f0a0672c4b4e15
vmoreli <victormoreli64@gmail.com>
2024-07-18 22:22:48 -0300

    Template base, navbar e home criados

--- gg_guess/gg_guess/urls.py
+++ gg_guess/gg_guess/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from gg_guess_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('gg/', include('gg_guess_app.urls')) # importing another urlconf
 ]
--- gg_guess/gg_guess_app/views.py
+++ gg_guess/gg_guess_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# views in django - request handlers (actions, in other frameworks)
+def home(request):
+    return render(request, "home.html")
