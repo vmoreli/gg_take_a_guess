23ab98860762fd4f688a10433c2ef6950f958faa
vmoreli <victormoreli64@gmail.com>
2024-07-24 17:10:57 -0300

    Corrigido erro de caixa de entrada após acertar

--- gg_guess/gg_guess/settings.py
+++ gg_guess/gg_guess/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gg_guess_app'
+    'gg_guess_app',
+    "django_rename_app"
 ]
 
 MIDDLEWARE = [
